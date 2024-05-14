5c1f81ab22087b87177d90e5b252439b21fa48ba
Angelina <zhmurkevych447@gmail.com>
2024-05-14 13:37:59 +0300

    Added files

---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int a = 10;
+        for (int b = 1; b <= a; b++){
+            if (a % b == 0){
+                System.out.print(b + " ");
+            }
+        }
+    }
+}
